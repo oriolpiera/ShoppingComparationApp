4ade97f62f84f2f3b7c3d7bdb035a8bb08e3dce2
Oriol Piera <oriol.piera@somenergia.coop>
2026-05-28 18:00:10 +0000

    fix(ui): add CRUD screens for supermarkets and products (#10)

--- lib/features/supermarkets/data/models/supermarket.dart
+++ lib/features/supermarkets/data/models/supermarket.dart
@@ -2,6 +2,12 @@ class Supermarket {
   int? id;
   String name;
   String? address;
+  bool isActive;
 
-  Supermarket({this.id, this.name = '', this.address});
+  Supermarket({
+    this.id,
+    this.name = '',
+    this.address,
+    this.isActive = true,
+  });
 }
