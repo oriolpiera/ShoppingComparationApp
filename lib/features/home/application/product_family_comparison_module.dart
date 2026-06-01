9b98cfa33cebeb837b37f07a261b76370ad56e46
Oriol Piera <oriol.piera@somenergia.coop>
2026-06-01 07:58:57 +0000

    Change default supermarket name to an empty string

--- lib/features/home/application/product_family_comparison_module.dart
+++ lib/features/home/application/product_family_comparison_module.dart
@@ -32,7 +32,7 @@ ProductFamilyComparisonView buildProductFamilyComparisonView({
     final supermarket = supermarketById[item.supermarketId];
     return ProductFamilyComparisonItem(
       productItem: item,
-      supermarketName: supermarket?.name ?? 'Unknown supermarket',
+      supermarketName: supermarket?.name ?? '',
       hasInactiveSupermarket: supermarket == null || !supermarket.isActive,
     );
   }).toList()
